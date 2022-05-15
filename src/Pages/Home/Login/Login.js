f909da968e291351dd60c27e7d597a45c8f0666b
gharamiananda <anandagharami.am@gmail.com>
2022-05-15 06:21:02 +0100

    hook form set up

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -7,7 +7,8 @@ import { useForm } from "react-hook-form";
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
     const onSubmit = data => console.log(data);
     return (
         <div>
@@ -17,20 +18,43 @@ const Login = () => {
 
                     <div class="card-body">
                         <h2 class="text-xl text-center">Login</h2>
-                        <div class="divider">OR</div>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            {/* register your input into the hook by invoking the "register" function */}
-                            <input defaultValue="test" {...register("example")} />
 
-                            {/* include validation with required or other standard HTML validation rules */}
-                            <input {...register("exampleRequired", { required: true })} />
-                            {/* errors will return when field validation fails  */}
-                            {errors.exampleRequired && <span>This field is required</span>}
-                            <input {...register("firstName", { required: true, maxLength: 20 })} />
+
+
+                        <form onSubmit={handleSubmit(onSubmit)}>
+
+
+                            <div class="form-control w-full max-w-xs">
+                                <label class="label">
+                                    <span class="label-text">your Email</span>
+
+                                </label>
+                                <input type="email"
+                                    placeholder="Type here"
+                                    class="input input-bordered w-full max-w-xs"
+                                    {...register("email", {
+                                        pattern: {
+                                            value: /[A-Za-z]{3}/,
+                                            message: 'error message' // JS only: <p>error message</p> TS only support string
+                                        }
+                                    })}
+                                />
+                                <label class="label">
+                                    <span class="label-text-alt">Alt label</span>
+
+                                </label>
+                            </div>
+
+
+                            <input {...register("firstName", { required: true, maxLength: 20 })}
+
+                            />
                             <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
                             <input type="number" {...register("age", { min: 18, max: 99 })} />
                             <input type="submit" />
                         </form>
+                        <div class="divider">OR</div>
+
 
                         <button class="btn btn-outline " onClick={() => signInWithGoogle()}>Continue With Google</button>
 
